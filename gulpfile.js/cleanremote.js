e0039a422a2faf0c51ba94a43743fc5ea0c319e2
Cherry Wang (Aquent LLC) <v-cherrw@microsoft.com>
2017-04-03 12:10:49 -0700

    Added Travis CI

---
+++ gulpfile.js/cleanremote.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const util = require('gulp-util');
+const minimist = require('minimist');
+const ftp = require('vinyl-ftp');
+
+const deployargs = minimist(process.argv.slice(2));
+const conn = ftp.create({
+  host: deployargs.host,
+  user: deployargs.user,
+  password: deployargs.password,
+  log: util.log,
+});
+
+gulp.task('cleanremote', (cb) => {
+  return conn.rmdir('**', function(err) {
+      cb();
+  });
+});
